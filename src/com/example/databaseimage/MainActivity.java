58f1e9b814086dc9eb6fa020f46cd9da638b9e94
RedTn <RED@RED-PC>
2014-10-03 00:04:35 -0700

    desktop

--- src/com/example/databaseimage/MainActivity.java
+++ src/com/example/databaseimage/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.databaseimage;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -12,34 +13,78 @@ public class MainActivity extends ActionBarActivity {
 
 	private DBHelper dbHelper = null;
 	//private Cursor ourCursor = null;
+	DBAdapter myDb;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        
         //create our database Helper
         dbHelper = new DBHelper(this);
         //we call the create right after initializing the helper, just in case
         //they have never run the app before
         dbHelper.createDatabase();
+        
+        openDB();
     }
     
-    private void displayText(String message) {
-    	TextView textView = (TextView) findViewById(R.id.editText1);
-    	textView.setText(message);
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    	
+    	closeDB();
     }
 
-    public void onClick_CLearAll(View v) {
+	private void openDB() {
+    	myDb = new DBAdapter(this);
+    	myDb.open();
+    }
+	
+	private void closeDB() {
+		myDb.close();
+	}
+    
+    private void displayText(String message) {
+    
+    }
+
+    public void onClick_ClearAll(View v) {
     	displayText("Clicked clear all");
     }
     
     public void onClick_DisplayRecords(View v) {
     	displayText("Clicked display record");
+    	
+    	Cursor cursor = myDb.getAllRecords();
+    	displayRecordSet(cursor);
     }
     
-    @Override
+    private void displayRecordSet(Cursor cursor) {
+		String message = "";
+		
+		
+		//Reset cursor to start
+		if (cursor.moveToFirst()) {
+			do {
+			int id = cursor.getInt(0);
+			String name = cursor.getString(1);
+			String title = cursor.getString(2);
+			
+			// Append data to the message
+			message += "id=" + id
+						+ ", name=" + name
+						+ ", title=" + title
+						+ "\n";
+			} while(cursor.moveToNext());
+		}
+		//Resource leak
+		cursor.close();
+		
+		displayText(message);
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
